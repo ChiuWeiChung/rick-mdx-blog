694e17311347ecc3085ba53214841f956f7509c5
rickchiu <rick@jgallop.com>
2025-12-14 14:01:49 +0800

    refactor: Simplify getOrigin function for improved clarity and maintainability

--- lib/router.ts
+++ lib/router.ts
@@ -3,10 +3,12 @@
 import { headers } from 'next/headers';
 
 export const getOrigin = async () => {
-  const headersList = await headers();
-  const fallbackProtocol = process.env.NODE_ENV === 'development' ? 'http' : 'https';
-  const protocol = headersList.get('x-forwarded-proto') ?? fallbackProtocol;
-  const host = headersList.get('host');
-  const origin = `${protocol}://${host}`;
-  return origin;
+  const h = await headers();
+
+  // NOTE: 用 x-forwarded-host + x-forwarded-proto 來還原外部網址：
+  const proto =
+    h.get('x-forwarded-proto') ?? (process.env.NODE_ENV === 'development' ? 'http' : 'https');
+
+  const host = h.get('x-forwarded-host') ?? h.get('host');
+  return `${proto}://${host}`;
 };
